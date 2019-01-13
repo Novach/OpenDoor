a24ffeac797389de82c51e680caaf1f596e4f35d
Novach <46598965+Novach@users.noreply.github.com>
2019-01-13 19:15:33 +0500

    Update Purchase.cs

--- PhoneMarket/Models/Purchase.cs
+++ PhoneMarket/Models/Purchase.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +8,8 @@ namespace PhoneMarket.Models
     public class Purchase
     {
         public int PurchaseID { get; set; }
+
+        public int PhoneId { get; set; }
 
         public DateTime DataTime { get; set; }
 
